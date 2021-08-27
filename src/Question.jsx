ef00f95a0e3ffc4519cc2ec52cdb038c50cb4e18
lee.pfeil <lee.pfeil@workday.com>
2021-08-27 13:02:21 -0700

    loading state

--- src/Question.jsx
+++ src/Question.jsx
@@ -3,14 +3,19 @@ import {Card} from "@workday/canvas-kit-react/card";
 import {SecondaryButton} from "@workday/canvas-kit-react/button";
 import QRCode from "qrcode.react";
 
-export default function Question(q, qr, handleUpvote) {
-    //const [qr, setQr] = React.useState(null);
-    console.log(qr);
+export default function Question(q, qr, initializeQr) {
+    const handleUpvote = () => {
+        initializeQr(q.id);
+        // TODO Show loading state
+    }
 
     return (
         <Card key={q.id}>
             <h4>{q.text}</h4>
-            <SecondaryButton className='upvote' onClick={() => handleUpvote(q.id)}>{q.dids.length} &#128588;</SecondaryButton>
+            <SecondaryButton
+                className='upvote'
+                onClick={handleUpvote}>{q.dids.length} &#128588;
+            </SecondaryButton>
 
             { qr === null ? null : (
                 <div>
